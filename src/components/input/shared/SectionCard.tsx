9e4627b6822fa6071f6015b347bb0ff211367acd
jaechi0601 <jaechi0601@gmail.com>
2026-04-12 13:02:18 +0900

    style: 인풋 섹션 타이틀 bold -> semibold

--- src/components/input/shared/SectionCard.tsx
+++ src/components/input/shared/SectionCard.tsx
@@ -41,7 +41,7 @@ export default function SectionCard({ title, subtitle, children, itemGap = 24 }:
             style={{
               margin: 0,
               fontSize: 24,
-              fontWeight: 700,
+              fontWeight: 600,
               color: 'var(--fig-title-color)',
               fontFamily: 'Pretendard, sans-serif',
               lineHeight: 1.5,
